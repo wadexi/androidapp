04dc6c68e6d0066635a418a88797c397002a9eec
wangdexi <wadexi2207@163.com>
2018-10-18 15:11:59 +0800

    gradle 配置变更，版本2 --app

--- app/src/main/java/com/example/a073105/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a073105/myapplication/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 		Log.i("git","version1");
+		Log.i("git","user2: version2");
     }
 }
